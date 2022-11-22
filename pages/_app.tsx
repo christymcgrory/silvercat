be455eb7250e3962549dc0a6febe6afeb74bdbfe
Christy McGrory <christyhughmcgrory@icloud.com>
2022-11-22 19:35:28 +0000

    initial commit. next.js init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Bambank</title>
+        <meta name="description" content="Welcome to Bambank" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
